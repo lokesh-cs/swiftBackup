41c515d2389f0f012e5d45062781057969fcfa90
Lokesh-cs <lokesh@contentsphere.com>
2016-09-19 20:52:51 +0530

    Removed unnecessary files

--- src/main/webapp/gui/mock/mock.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-
-    pieChartColor: {
-        pass: 'rgba(10, 193, 10, 0.83)',
-        fail: 'rgba(245, 35, 35, 0.83)',
-        inProgress: 'rgba(236, 236, 27, 0.85)'
-    }
-
-};
